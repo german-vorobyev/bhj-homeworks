899f8764802fd3c8fcbaa6bd398ade555f30ec39
german-vorobyev <german.v93@mail.ru>
2020-11-08 13:28:52 +0300

    Second commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -9,11 +9,13 @@ menu__link.forEach(function(item) {
     }
 });
 menu_item.forEach(function(item) {
-    item.addEventListener("click", function() {
+    item.addEventListener("click", function(target) {
         let menu_main = this.closest(".menu_main");
         let menu_active = menu_main.querySelector(".menu_active");
         if (menu_active)
-        menu_active.classList.toggle("menu_active");
+            menu_active.classList.toggle("menu_active");
+        if (menu_active != null && menu_active.parentElement == target.currentTarget)
+            return;     
         let menu_sub = this.querySelector(".menu_sub");
         if (menu_sub) {
             menu_sub.classList.toggle("menu_active");
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,37 +1,56 @@
 let slider__arrow_prev = document.querySelector(".slider__arrow_prev");
 let slider__arrow_next = document.querySelector(".slider__arrow_next");
-slider__arrow_next.addEventListener("click", function() {
-    let slider__item_active = document.querySelector(".slider__item_active");
-    slider__item_active.classList.toggle("slider__item_active");
-    let next_slider_item_active = slider__item_active.nextElementSibling;
-    if (!next_slider_item_active) {
-        next_slider_item_active = slider__item_active.parentElement.firstElementChild;
-    }
-    next_slider_item_active.classList.toggle("slider__item_active");
-});
-slider__arrow_prev.addEventListener("click", function() {
-    let slider__item_active = document.querySelector(".slider__item_active");
-    slider__item_active.classList.toggle("slider__item_active");
-    let prev_slider_item_active = slider__item_active.previousElementSibling;
-    if (!prev_slider_item_active) {
-        prev_slider_item_active = slider__item_active.parentElement.lastElementChild;
-    }
-    prev_slider_item_active.classList.toggle("slider__item_active");
-});
 let listImages = document.querySelectorAll(".slider__item");
 let listDots = document.querySelectorAll(".slider__dot");
-let arrayImages = [];
-listImages.forEach(function (item) {
-    arrayImages.push(item);
+let arrayImages = Array.from(listImages);
+let arrayDots = Array.from(listDots);
+function showImage (index) {
+    let slider__item_active = document.querySelector(".slider__item_active");
+    slider__item_active.classList.toggle("slider__item_active");
+    arrayImages[index].classList.toggle("slider__item_active");
+}
+function blinkDot (index) {
+    let slider__dot_active = document.querySelector(".slider__dot_active");
+    if (slider__dot_active) {
+        slider__dot_active.classList.toggle("slider__dot_active");
+    }
+    arrayDots[index].classList.toggle("slider__dot_active");
+}
+function slideImages (index) {
+    showImage(index);
+    blinkDot(index);
+}
+blinkDot(0);
+let controller = {
+    index: 0,
+    arrayImages: arrayImages,
+    showImage: function(index) {},
+    nextImage: function() {
+        this.index = (this.index + 1) % this.arrayImages.length;
+        this.showImage(this.index);
+    },
+    prevImages: function() {
+        this.index = this.index - 1;
+        if (this.index < 0)
+            this.index = this.arrayImages.length - 1;
+        this.showImage(this.index);
+    },
+    setIndex: function(index) {
+        this.index = index;
+        this.showImage(this.index);
+    } 
+};
+controller.showImage = slideImages;
+slider__arrow_next.addEventListener("click", function() {
+    controller.nextImage();
 });
-let arrayDots = [];
-listDots.forEach(function(item) {
-    arrayDots.push(item);
+
+slider__arrow_prev.addEventListener("click", function() {
+    controller.prevImages();
 });
+
 arrayDots.forEach(function(item,index) {
     item.addEventListener("click" ,function() {
-        let slider__item_active = document.querySelector(".slider__item_active");
-        slider__item_active.classList.toggle("slider__item_active");
-        arrayImages[index].classList.toggle("slider__item_active");
-    })
+        controller.setIndex(index);
+    });
 });
